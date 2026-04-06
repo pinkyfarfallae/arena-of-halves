87d4aa7e6fcb06e00de1b62314b18e5e21b4c613
pinkyfarfallae <fancykeith.sarang@gmail.com>
2026-04-06 14:29:09 +0700

    Hebe & Hecate

---
+++ src/services/irisWish/applyWishesEffect.ts
@@ -0,0 +1,20 @@
+import { BAG_ITEM_TYPES } from "../../constants/bag";
+import { DEITY } from "../../constants/deities";
+import { Wish } from "../../types/wish";
+import { giveItem } from "../bag/bagService";
+import { updateTrainingPoints } from "../training/trainingPoints";
+
+export const applyWishesEffect = (wish: Wish, characterId: string) => {
+  const { deity } = wish;
+  switch (deity) {
+    case DEITY.HEBE:
+      const item = { itemId: 'health_potion_s', quantity: 1, type: BAG_ITEM_TYPES.ITEM };
+      giveItem(characterId, item.itemId, item.quantity, item.type);
+      break;
+    case DEITY.HECATE:
+      updateTrainingPoints(characterId, 1);
+      break;
+    default:
+      break;
+  }
+};
