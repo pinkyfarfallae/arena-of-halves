969d4e2c0f647dfeab3cd8661502f68ed98d3184
pinkyfarfallae <fancykeith.sarang@gmail.com>
2026-04-17 03:27:20 +0700

    feat: reorganize item constants for better structure and clarity

--- src/constants/items.ts
+++ src/constants/items.ts
@@ -1,8 +1,31 @@
 export const ITEMS = {
   SHOP_30_DISCOUNT_TICKET: '30_discount_ticket',
+
   HEALTH_POTION_S: 'health_potion_s',
-  UPGRADE_GUARANTEE_TICKET: 'upgrade_guarantee_ticket',
-  ATHENA_S_CODEX: 'athenas_codex',
-  REFUND_SKILL_TICKET: 'refund_skill_ticket',
+  HEALTH_POTION_M: 'health_potion_m',
+  HEALTH_POTION_L: 'health_potion_l',
+
+  WRAITH_POTION: 'wraith_potion',
+  LUCKY_POTION: 'lucky_potion',
+  SWIFTNESS_POTION: 'swiftness_potion',
+
+  AMBROSIA: 'ambrosia',
+  NECTAR: 'nectar',
+
   NEMESIS_S_JUSTICE_COOKIE: 'nemesiss_justice_cookie',
+
+  CAMP_TSHIRT: 'camp_tshirt',
+  QUEST_KEEPER: 'quest_keeper',
+
+  NIKE_S_STATUE: 'nikes_statue',
+  HERMES_S_PURSE: 'hermess_purse',
+
+  RAINBOW_DRACHMA: 'rainbow_drachma',
+
+  SKIP_TICKET: 'skip_ticket',
+  UPGRADE_GUARANTEE_TICKET: 'upgrade_guarantee_ticket',
+  REFUND_SKILL_TICKET: 'refund_skill_ticket',
+
+  ATHENA_S_CODEX: 'athenas_codex',
+  DEMETER_S_GARDENING_SET: 'demeters_gardening_set',
 }
