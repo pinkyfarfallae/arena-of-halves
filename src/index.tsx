75d459da46cb723efb23ab29a9ca47e97e71f4ac
pinkyfarfallae <fancykeith.sarang@gmail.com>
2026-05-25 15:48:40 +0700

    feat: remove version check utility and related logic from the application

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { startVersionCheck, clearServiceWorkers } from './utils/versionCheck';
-
-// Clear old service workers and start version checking
-clearServiceWorkers();
-startVersionCheck();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
